34c07504a4d1589b80f31f66525d5cc21d68ea81
chamaobird <lgpassarini@gmail.com>
2026-03-20 10:58:19 -0400

    arrumando estrutura de players ativos et3

--- alembic/versions/20260320_1418_b67417853848_add_is_active_to_players.py
+++ alembic/versions/20260320_1418_b67417853848_add_is_active_to_players.py
@@ -8,6 +8,7 @@ Create Date: 2026-03-20 14:18:54.260640+00:00
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import inspect
 
 revision: str = 'b67417853848'
 down_revision: Union[str, None] = '0007'
@@ -16,12 +17,16 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column('players', sa.Column(
-        'is_active',
-        sa.Boolean(),
-        nullable=False,
-        server_default=sa.text('true')
-    ))
+    conn = op.get_bind()
+    inspector = inspect(conn)
+    columns = [col['name'] for col in inspector.get_columns('players')]
+    if 'is_active' not in columns:
+        op.add_column('players', sa.Column(
+            'is_active',
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.text('true')
+        ))
 
 
 def downgrade() -> None:
